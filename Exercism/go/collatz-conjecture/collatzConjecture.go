a286cd469a37f5306f22d1dc135656fbfbdd6929
gnana03 <gnana.kilambhi@imaginea.com>
2019-09-25 19:50:38 +0530

    Collatz Conjecture is solved

---
+++ Exercism/go/collatz-conjecture/collatzConjecture.go
@@ -0,0 +1,20 @@
+package collatzconjecture
+import "errors"
+
+func CollatzConjecture(input int) (int,error){
+   counter:=0
+   if input<=0{
+	 return input,errors.New("Invalid value provided")
+   }else{
+   for input>1{
+     if input%2==0{
+        input=input/2
+     } else{
+      input=(3*input)+1
+    }
+   counter=counter+1
+   }
+   return counter,nil
+   }
+   }
+
